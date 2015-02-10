33e5437445ba5653d0fade4a4888b89e99d4b24a
Adamssss <gleek117.l@gmail.com>
2015-02-09 21:24:39 -0600

    problem 7
    a very slow but right solution to problem 7

---
+++ pb7.py
@@ -0,0 +1,23 @@
+a = [2,3,5]
+n = 2
+while n < 10000:
+    b = a[n]
+    t = 1
+    while (t == 1):
+        b = b+2
+        i = 0
+        t = 0
+        while (i < n):
+            i=i+1
+            if (b%a[i] == 0):
+                t = 1
+
+                
+        if (t == 0):
+            n = n+1
+            a.append(b)
+
+print (b)
+
+
+                
