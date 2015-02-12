eae78d80c3cf455361921c1d68d1833b7d1f2705
Adamssss <gleek117.l@gmail.com>
2015-02-12 14:00:58 -0600

    pb 18-20

---
+++ pb020.py
@@ -0,0 +1,13 @@
+# factor
+def fact(x):
+    if x == 1:
+        return 1
+    if x > 1:
+        return x*fact(x-1)
+
+number = str(fact(100))
+sumOfDigits = 0
+for i in range(0,len(number)):
+    sumOfDigits += ord(number[i])-48
+
+print (sumOfDigits)
