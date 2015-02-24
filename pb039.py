8bc7482fca4a88fa61fbf4d9e19c56baf4cae4dc
Adamssss <gleek117.l@gmail.com>
2015-02-23 21:48:49 -0600

    pb 39

---
+++ pb039.py
@@ -0,0 +1,19 @@
+# a <= b <= c
+# a <= p/3
+# b <= p/2
+
+largest = 0
+largestp = 0
+for p in range(12,1001):
+    count = 0
+    for i in range(1,p//3):
+        for j in range(i,p//2):
+            c = p-i-j
+            if i*i+j*j-c*c == 0:
+                count += 1
+    
+    if count > largest:
+        largest = count
+        largestp = p
+
+print (largestp)
