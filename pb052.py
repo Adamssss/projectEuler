348e44c6187eb77281181b1660c3c5473cb4fd53
Adamssss <gleek117.l@gmail.com>
2015-03-01 02:05:59 -0600

    the proper one for 52

--- pb052.py
+++ pb052.py
@@ -4,3 +4,42 @@
 
 print(142857)
 
+# okay I'm gonna pretend I don't know it and do it properly
+
+import math
+
+def digs(num):
+    return math.floor(math.log10(num)+1)
+
+# define a method to change the number into a sequence that represent the digits
+# for example : 998642 would be 2101010100
+# the fisrt two stands for two nines and the following ones stand for 2,4,6,8
+def comp(num):
+    temp = num
+    result = 0
+    while temp > 0:
+        n = temp%10
+        temp = temp//10
+        result += math.pow(10,n)
+    return result
+
+# print(comp(998642))
+
+i = 0
+while True:
+    i += 1
+    if digs(6*i) > digs(i):
+        continue
+    target = comp(i)
+    if comp(2*i) != target:
+        continue
+    if comp(3*i) != target:
+        continue
+    if comp(4*i) != target:
+        continue
+    if comp(5*i) != target:
+        continue
+    if comp(6*i) != target:
+        continue
+    print (i)
+    break
