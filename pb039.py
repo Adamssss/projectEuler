d84d931a6ac0202eb3ee31f271a161be4608a8ba
Adamssss <gleek117.l@gmail.com>
2015-03-12 01:55:53 -0500

    39 2.0

--- pb039.py
+++ pb039.py
@@ -6,15 +6,22 @@ t1 = time.time()
 # a <= p/3
 # b <= p/2
 
+# a^2+b^2 = (p-a-b)^2
+#0=p^2+2ab-2ap-2bp
+#b = p^2-2ap/2p-2a
+
+#for each set of a & p
+# at most one b can be found
+
 largest = 0
 largestp = 0
 for p in range(12,1001,2):
     count = 0
     for i in range(1,p//3):
-        for j in range(i,p//2):
-            c = p-i-j
-            if i*i+j*j-c*c == 0:
-                count += 1
+        n = p*p-2*i*p
+        d = 2*p-2*i
+        if n%d == 0:
+            count += 1
     
     if count > largest:
         largest = count
