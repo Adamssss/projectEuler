a786fac9a1dda9808ce27b8ea65aab5ef4a18bb7
Adamssss <gleek117.l@gmail.com>
2015-03-23 00:20:39 -0500

    faster 76

--- pb076.py
+++ pb076.py
@@ -2,22 +2,24 @@ import time
 
 t1 = time.time()
 
-# i-1 kinds of number make of i
-# f(i,j) = f(i-1,j)+f(i,j-Pi)
+N = 100
 
-def f(i,j):
-    if i == 1:
-        return 1
-    if i == 0:
-        return 0
-    if j < 0:
-        return 0
-    if j == 0:
-        return 1
-    if i > j:
-        return f(j,j)
-    return f(i-1,j)+f(i,j-i)
+w = [0]*(N+1)
+w[0] = 1
 
-print(f(99,100))
+for i in range(1,N+1):
+    # i stands for the numbers used form 1 to i
+    for j in range(i,N+1):
+        # w[j] is the ways make out of 1 to i-1
+        # when i is added to the numbers
+        # j equals i and j-i
+        # so w[j-i] is added to w[j]
+        w[j] += w[j-i]
+
+def f(i):
+    return w[i]-1
+
+print(f(N))
+    
 
 print("time:",time.time()-t1)
