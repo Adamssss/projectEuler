7d78ef1a8f41a2ee508f2f12d8aa3a1d1dd0ca9f
Adamssss <gleek117.l@gmail.com>
2015-03-28 01:13:24 -0500

    faster 43

--- pb043.py
+++ pb043.py
@@ -16,17 +16,20 @@ def QPL(m_list):
 
 divider = [1,2,3,5,7,11,13,17]
 total = 0
-for i in QPL([9,8,7,6,5,4,3,2,1,0]):
-    t = 1
-    for j in range(1,8):
-        if (i[j]*100+i[j+1]*10+i[j+2])%divider[j] > 0:
-            t = 0
 
-    if t == 1:
-        num = 0
-        for j in range(len(i)):
-            num = num *10 + i[j]
-        total += num
+def addif(lst):
+    if lst[3]%2 != 0:
+        return 0
+    for j in range(2,8):
+        if (lst[j]*100+lst[j+1]*10+lst[j+2])%divider[j] > 0:
+            return 0
+    num = 0
+    for j in range(len(i)):
+        num = num *10 + lst[j]
+    return num
+    
+for i in QPL([9,8,7,6,5,4,3,2,1,0]):
+    total += addif(i)
         
 print (total)
 
