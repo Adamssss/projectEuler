419f65a6a6bb3594588303c9095ae28ca050e422
Adamssss <gleek117.l@gmail.com>
2015-03-31 00:43:25 -0500

    faster 51

--- pb051.py
+++ pb051.py
@@ -3,25 +3,22 @@ import time
 
 t1 = time.time()
 
-prime = [2,3,5]
-primen = 2
+n = 1000000
 
-while prime[primen] < 1000000:
-    b = prime[primen]
-    t = 1
-    while (t == 1):
-        b = b+2
-        i = 0
-        t = 0
-        while (prime[i]*prime[i] < b)and (t == 0):
-            i=i+1
-            if (b%prime[i] == 0):
-                t = 1
-               
-        if (t == 0):
-            primen += 1
-            prime.append(b)
+p = [True]*(n)
 
+i = 1
+prime = [2]
+
+while i < n:
+    if p[i]:
+        t = 2*i+1
+        prime.append(t)
+        j = i
+        while j < n:
+            p[j] = False
+            j += t
+    i += 1
 
 def isPrime(item):
     root = math.floor(math.sqrt(item))
