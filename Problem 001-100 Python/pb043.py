99c4e4dbc1763b104eec7346d8843f4daabeabec
Adamssss <gleek117.l@gmail.com>
2015-04-22 11:30:19 -0500

    faster 43

--- Problem 001-100 Python/pb043.py
+++ Problem 001-100 Python/pb043.py
@@ -2,7 +2,6 @@ import time
 
 t1 = time.time()
 
-# code QPL generator copied from pb 41
 def QPL(m_list):
     if len(m_list) == 1:
         return [m_list]
@@ -14,23 +13,49 @@ def QPL(m_list):
             qpl.append([i]+j)
     return qpl
 
-divider = [1,2,3,5,7,11,13,17]
-total = 0
+total = [0,0,0,0,0,0,0,0,0,0]
 
-def addif(lst):
+def satisfy(lst):
+    temp = lst[5] + lst[7] - lst[6]
+    if  temp != 0 and temp != 11:
+        return False
     if lst[3]%2 != 0:
-        return 0
-    for j in range(2,8):
-        if (lst[j]*100+lst[j+1]*10+lst[j+2])%divider[j] > 0:
-            return 0
-    num = 0
-    for j in range(len(i)):
-        num = num *10 + lst[j]
-    return num
-    
-for i in QPL([9,8,7,6,5,4,3,2,1,0]):
-    total += addif(i)
+        return False
+    temp = lst[2]+lst[3]+lst[4]
+    if temp%3 != 0:
+        return False
+    temp = lst[6]*100+lst[7]*10+lst[8]
+    if temp%13 > 0:
+        return False
+    temp = (temp%100)*10+lst[9]
+    if temp%17 > 0:
+        return False
+    temp = lst[4]*100+lst[5]*10+lst[6]
+    if temp%7 > 0:
+        return False
+    return True
+
+def add(lsta,lstb):
+    for i in range(10):
+        lsta[i] += lstb[i]
+    return lsta
+
+def tonumber(lst):
+    result = 0
+    for i in range(10):
+        result = result*10+lst[i]
+    return result
+
+for i in QPL([9,8,7,6,4,3,2,1,0]):
+    i.insert(5,5)
+    if satisfy(i):
+        total = add(total,i)
+
+for i in QPL([9,8,7,6,5,4,3,2,1]):
+    i.insert(5,0)
+    if satisfy(i):
+        total = add(total,i)
         
-print (total)
+print (tonumber(total))
 
 print("time:",time.time()-t1)
