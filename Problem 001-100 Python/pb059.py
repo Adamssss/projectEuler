ce4d60bac57998bd486a7bf8e89dac8bb7c55f93
Adamssss <gleek117.l@gmail.com>
2015-12-04 16:08:59 -0600

    faster 59

--- Problem 001-100 Python/pb059.py
+++ Problem 001-100 Python/pb059.py
@@ -9,18 +9,21 @@ ciphers = f.read().split(',')
 
 f.close()
 
-for i in range(0,len(ciphers)-1):
-    temp = 0
-    for j in range(0,len(ciphers[i])):
-        dig = ord(ciphers[i][j]) -48
-        temp = temp*10 + dig
-    ciphers[i] = temp
+last = ciphers[-1][:-1]
+ciphers = ciphers[:-1]
+ciphers.append(last)
 
-temp = 0
-for j in range(0,len(ciphers[-1])-1):
-    dig = ord(ciphers[-1][j]) -48
-    temp = temp*10 + dig
-ciphers[-1] = temp
+def tonumber(s):
+    r = []
+    for i in s:
+        temp = 0
+        for j in i:
+            dig = ord(j)-48
+            temp = temp*10 + dig
+        r.append(temp)
+    return r
+        
+ciphers = tonumber(ciphers)
 
 def decrypt(key):
     k = 0
@@ -30,14 +33,12 @@ def decrypt(key):
         k += 1
         if k == 3:
             k = 0
-
     return temp
 
 def show(cipher):
     line = ""
     for i in range(0,len(cipher)):
         line += chr(cipher[i])
-
     print(line)
 
 def english(cipher):
@@ -50,7 +51,11 @@ def english(cipher):
     xxs = 0
     zzs = 0
     letters = len(cipher)
-    for i in range(0,letters):
+    jjl = letters*0.02
+    qql = letters*0.01
+    xxl = letters*0.01
+    zzl = letters*0.01
+    for i in range(letters):
         if cipher[i] == 65 or cipher[i] == 97:
             aas += 1
         if cipher[i] == 69 or cipher[i] == 101:
@@ -59,14 +64,23 @@ def english(cipher):
             iis += 1
         if cipher[i] == 79 or cipher[i] == 111:
             oos += 1
+            
         if cipher[i] == 74 or cipher[i] == 106:
             jjs += 1
+            if jjs > jjl:
+                return False
         if cipher[i] == 81 or cipher[i] == 113:
             qqs += 1
+            if qqs > qql:
+                return False
         if cipher[i] == 88 or cipher[i] == 120:
             xxs += 1
+            if xxs > xxl:
+                return False
         if cipher[i] == 90 or cipher[i] == 122:
             zzs += 1
+            if zzs > zzl:
+                return False
     
     if aas/letters < 0.04:
         return False
@@ -76,15 +90,7 @@ def english(cipher):
         return False
     if oos/letters < 0.05:
         return False
-    if jjs/letters > 0.02:
-        return False
-    if qqs/letters > 0.01:
-        return False
-    if xxs/letters > 0.01:
-        return False
-    if zzs/letters > 0.01:
-        return False
-    
+
     return True
     
 def sumOf(cipher):
@@ -92,16 +98,20 @@ def sumOf(cipher):
     for i in range(0,len(cipher)):
         total += cipher[i]
     print (total)
-          
-for i in range(97,123):
-    for j in range(97,123):
-        for k in range(97,123):
-            temp = decrypt([i,j,k])
-            if english(temp):
-                #print(i,j,k)
-                #show(temp)
-                sumOf(temp)
-                
+
+def getkey():
+    for i in range(97,123):
+        for j in range(97,123):
+            for k in range(97,123):
+                temp = decrypt([i,j,k])
+                if english(temp):
+                    #print(i,j,k)
+                    #show(temp)
+                    sumOf(temp)
+                    return
+
+getkey()
+         
 print("time:",time.time()-t1)
 
 
