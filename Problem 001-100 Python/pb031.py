bb987d2631728b1424c18afc67a63235646b883e
Adamssss <gleek117.l@gmail.com>
2016-01-21 18:39:47 -0600

    dp 31

--- Problem 001-100 Python/pb031.py
+++ Problem 001-100 Python/pb031.py
@@ -5,21 +5,25 @@ t1 = time.time()
 # f(i,j) = f(i-1,j)+f(i,j-Pi)
 # i kinds of notes to make up money equals j
 
+# dynamic programming 
 
 coins = [1,2,5,10,20,50,100,200]
 
-def f(i,j):
-    if i == 1:
-        return 1
-    if i == 0:
-        return 0
-    if j < 0:
-        return 0
-    if j == 0:
-        return 1
-    return f(i-1,j)+f(i,j-coins[i-1])
+f =[]
+f += [[0]*201]
+f += [[1]*201]
 
-    
-print (f(8,200))
+for i in range(7):
+    f += [[0]*201]
+
+for i in range(2,9):
+    c = coins[i-1]
+    f[i][0] = 1
+    for j in range(1,c):
+        f[i][j] = f[i-1][j]
+    for j in range(c,201):
+        f[i][j] = f[i-1][j]+f[i][j-c]
+
+print(f[8][200])
 
 print("time:",time.time()-t1)
